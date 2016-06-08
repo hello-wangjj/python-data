bb03051b0cef2f6e39fbd178ffd3a80487330f9d
wangjj_win <576988736@qq.com>
2016-06-08 15:36:54 +0800

    week1

---
+++ week1/ex1.py
@@ -0,0 +1,6 @@
+#-*-:coding:utf-8-*-
+surname=raw_input('Input your surname:')
+firstname=raw_input('Input your firstname:')
+print 'Your surname is:\n',surname
+print 'Your firstname is:\n',firstname
+print 'Your fullname is:\n',surname,firstname
